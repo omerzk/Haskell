01b03fff5ddc21c309a953894a2cf5a491ef242b
omerzk <omeryom@gmail.com>
2015-03-12 20:48:49 +0200

    middle of subSets

--- allkinds.hs
+++ allkinds.hs
@@ -1,15 +1,15 @@
-gcd :: (Integral a) => a -> a -> a
+gcd' :: (Integral a) => a -> a -> a
 
-gcd m n 
+gcd' m n 
 	| n == 0 = m
-	| otherwise = gcd n (m 'mod' n)
+	| otherwise = gcd n (m `mod` n)
 
 --------------------------------------------------------------------------
-insertIn ::(Integral a) => a -> b -> [b] -> [c] 
+insertIn :: Int -> b -> [b] -> [b] 
 
 insertIn i e xs
   | i == 0 = e:xs 
-  | i > length(xs) = "you'r an idiot" 
+  | i > length(xs) = []
   | otherwise = head xs : (insertIn (i-1) e (tail xs))
 ---------------------------------------------------------------------------
 lastNitems :: Int -> [a] -> [a]
@@ -17,23 +17,36 @@ lastNitems :: Int -> [a] -> [a]
 lastNitems n xs
     | n > length xs = []
     | n == length xs = xs
-    | otherwise = lastNitems (tail xs)
---.........................................
+    | otherwise = lastNitems n (tail xs)
+--------------------------------------------------------------------------
 lastNitems' n xs = take n (reverse xs)
 ---------------------------------------------------------------------------
-slice :: ->Int -> [a]-> ([a], [a])
+slice :: Int -> [a]-> ([a], [a])
 
 slice k xs
     | k < 0 = ([],xs) 
     | k > length xs = (xs,[]) 
-    | otherwise = ((take k xs),lastNitems ((length xs) - k))
+    | otherwise = ((take k xs), lastNitems ((length xs) - k) xs)
 --------------------------------------------------------------------------
 runLength :: (Eq a) => [a] -> [(a, Int)]
+run :: (Eq a) => [a] -> [a]
 
-run :: (Eq a) => [a] -> Int
- 
-runLength sq
-    | sq == [] = []
-    | head sq == (head (tail sq)) = (1 + (fst (runLength (tail sq))), head sq) : 
-    | otherwise = 1 
+run sq 
+    | tail sq == [] = [] 
+    | head sq == (head (tail sq)) = run (tail sq) 
+    | otherwise = tail sq
 
+runLength sq 
+    | sq == [] = [] 
+    |otherwise = (head sq, (length sq) - (length r)) : runLength r 
+    where r = run sq 
+----------------------------------------------------------------------------
+subSets :: (Eq a , Integral b) => b -> [a] -> [[a]]
+
+subSets k set = 
+    | k > len || k == 0 = []
+    | k == len = set 
+    | head set : subSets (k - 1) (tail set):[] ++ subSets k (tail set)
+    where len = length set
+
+main = do putStrLn (show (runLength ['a','a','a','b','b'])) 
